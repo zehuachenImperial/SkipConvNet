c4f94be2f2ad99c3f08abb7059e0010ff70fbdd6
vkothapally <vinay.kothapally@utdallas.edu>
2020-08-13 17:31:55 -0400

    Added Train script

--- train.py
+++ train.py
@@ -1,9 +1,24 @@
-import os
+import os, argparse
 from model import SkipConvNet
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,4"
-skipconv_model = SkipConvNet()
-trainer = pl.Trainer(max_epochs=40, gpus=3, distributed_backend='ddp')    
-trainer.fit(skipconv_model)   
-trainer.test()
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(description='Train SkipConvNet Model for Single Channel Speech Dereverberation (Interspeech 2020)')
+	parser.add_argument('--epocs',    type=int,   help='Max number of epocs       (default: 15)', default=15)
+	parser.add_argument('--ngpus',    type=int,   help='Max number of GPUs to use (default:  3)', default=3)
+	parser.add_argument('--precision',type=int,   help='Precision for Loss        (default: 16)', default=16)
+	parser.add_argument('--logdir',   type=str,   help='Tensorflow logdir name    (default: tfLogs)', default='tfLogs')
+	parser.add_argument('--chkptdir', type=str,   help='Checkpoints dir name      (default: chkpts)', default='chkpts')
+	args = parser.parse_args()
+	
+	skipconv_model = SkipConvNet()
+	checkpoint_callback = ModelCheckpoint(filepath=os.getcwd()+'/'+args.chkptdir, save_top_k=True, verbose=True, monitor='val_loss',mode='min',prefix='')
+
+	logger = TensorBoardLogger(save_dir=os.getcwd(), version=1, name=args.logdir)
+	trainer = pl.Trainer(max_epochs=40, gpus=3, distributed_backend='ddp',logger=logger, precision=args.precision, checkpoint_callback=ModelCheckpoint())  
+	trainer.fit(skipconv_model)   
+	trainer.test()
