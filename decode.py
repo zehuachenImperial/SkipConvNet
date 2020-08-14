531ba4f87b4a2ea7a2c22aeb670efc24d5330556
vkothapally <vinay.kothapally@utdallas.edu>
2020-08-14 14:11:00 -0400

    Added Decode script

--- decode.py
+++ decode.py
@@ -5,7 +5,7 @@ Created on Mon Jan 20 14:58:27 2020
 
 @author: vinay
 """
-import os, sys
+import os, argparse
 import numpy as np
 import torch
 import torch.nn as nn
@@ -29,76 +29,49 @@ def __readscpFiles__(filename):
     return fileList
 
 
-def getfileLocs(reverbscp, wpescp, enhanceName):
+def deocdeData(dataloc, specImageloc, destfolder, model, device): 
+	for dataset in ['Dev', 'Eval']:
+		audiofiles  = __readscpFiles__(dataloc+'/'+dataset+'_SimData.scp')
+		audiofiles.update(__readscpFiles__(dataloc+'/'+dataset+'_RealData.scp'))
+		pbar = pkbar.Pbar(name='Decoding '+dataset+' AudioFiles: ', target=len(audiofiles))
+		
+		data = SpecImages(specImageloc+'/'+dataset, mode='decode')
+		with torch.no_grad():
+			for i, (k, v) in enumerate(audiofiles.items()):
+				uttID = data.uttname2idx('MagdB_'+k)
+				audio = data.__getaudio__(uttID)
+				input_mag     = audio['noisy_mag'].unsqueeze(1).to(device)
+				enhanced_mag  = model(input_mag).cpu().numpy()
+				if enhanced_mag.shape[0]>1:
+					enhanced_mag  = np.hstack(np.squeeze(enhanced_mag))
+				else:
+					enhanced_mag  = np.squeeze(enhanced_mag)
+				enhanced_mag  = np.interp(enhanced_mag, [-1,1],audio['noisy_norm'])
+				temp = np.zeros((257, enhanced_mag.shape[1])) + 1j*np.zeros((257, enhanced_mag.shape[1]))
+				temp[:-1,:] = 10**(enhanced_mag/20) * (np.cos(audio['noisy_phase']) + np.sin(audio['noisy_phase'])*1j)
+				enhanced_audio = istft(temp)
+				enhanced_audio = 0.98*enhanced_audio/np.max(np.abs(enhanced_audio))
+				enhanced_audio = enhanced_audio[:audio['utt_samples']]
 
-	filelocs   = {'uttname':[], 'reverbloc':[], 'wpeloc':[], 'enhanceloc':[]}
-	reverblist = __readscpFiles__(reverbscp)
-	wpelist    = __readscpFiles__(wpescp)
+				destloc = destfolder+v.split('Reverb_Challenge')[1]
+				Path(os.path.dirname(destloc)).mkdir(parents=True, exist_ok=True)
+				sf.write(destloc, enhanced_audio,   16000)
+				del audio,input_mag,enhanced_mag,temp,enhanced_audio
+				pbar.update(i)
 
-	for utt in reverblist.keys():
-		filelocs['uttname'].append(utt)
-		filelocs['reverbloc'].append(reverblist[utt])
-		filelocs['wpeloc'].append(wpelist[utt])
-		filelocs['enhanceloc'].append(wpelist[utt].replace('WPE', enhanceName))
-
-	filelocs = pd.DataFrame(filelocs)
-	return filelocs
-
-
-
-datasetloader   = SpecImages('/data/scratch/vkk160330/Features/Reverb_Spec/1ch/Dev', mode='decode')
-# def deocdeData(model, device, destfolder):
-
-
-
-
-# def decode(dataset, model, device, destfolder):
-# 	kaldidir = '/erasable/dataset/REVERB/kaldi_reverb/vinay_s5/data_se/'
-# 	filelocs = getfileLocs(kaldidir+dataset+'/wav.scp', kaldidir+dataset+'_wpe/wav.scp', destfolder)
-# 	if 'dt' in dataset:
-# 		datasetloader   = ReverbChallenge_Images('/data/scratch/vkk160330/Features/Reverb_Spec/1ch/Dev', mode='decode')
-# 	elif 'et' in dataset:
-# 		datasetloader   = ReverbChallenge_Images('/data/scratch/vkk160330/Features/Reverb_Spec/1ch/Eval', mode='decode')
-
-# 	with torch.no_grad():
-# 		pbar = pkbar.Pbar(name='Decoding : '+dataset, target=len(filelocs))
-# 		for i, row in filelocs.iterrows():
-# 			audio = datasetloader.__evalaudio__(row['uttname'])
-# 			input_mag     = audio['noisy_mag'].unsqueeze(1).to(device)
-# 			enhanced_mag  = model(input_mag).cpu().numpy()
-# 			if enhanced_mag.shape[0]>1:
-# 				enhanced_mag  = np.hstack(np.squeeze(enhanced_mag))
-# 			else:
-# 				enhanced_mag  = np.squeeze(enhanced_mag)
-# 			enhanced_mag  = np.interp(enhanced_mag, [-1,1],audio['noisy_norm'])
-# 			temp = np.zeros((257, enhanced_mag.shape[1])) + 1j*np.zeros((257, enhanced_mag.shape[1]))
-# 			temp[:-1,:] = 10**(enhanced_mag/20) * (np.cos(audio['noisy_phase']) + np.sin(audio['noisy_phase'])*1j)
-# 			enhanced_audio = istft(temp)
-# 			enhanced_audio = 0.98*enhanced_audio/np.max(np.abs(enhanced_audio))
-# 			enhanced_audio = enhanced_audio[:audio['utt_samples']]
-# 			Path(os.path.dirname(row['enhanceloc'])).mkdir(parents=True, exist_ok=True)
-# 			sf.write(row['enhanceloc'], enhanced_audio,   16000)
-# 			del audio,input_mag,enhanced_mag,temp,enhanced_audio
-# 			pbar.update(i)
-# 	return
-
-
-
-# def main():
-# 	use_cuda  = torch.cuda.is_available()
-# 	device    = torch.device('cuda' if use_cuda else 'cpu')
-# 	dtype     = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-# 	model = SkipConvNet().to(device)
-# 	saved_model = torch.load('./lightning_logs/version_0/checkpoints/epoch=21.ckpt')
-# 	model.load_state_dict(saved_model['state_dict'])
-# 	model.eval()
-# 	destfolder= 'SkipConvNet'
-# 	print('Decoding audiofiles')
-# 	for dataset in ['dt_real_1ch', 'et_real_1ch', 'dt_simu_1ch', 'et_simu_1ch']:
-# 		decode(dataset, model, device,destfolder)
-
-
-# if __name__ == "__main__":
-#     main()
-#     print('Done!')
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(description='Decode Single Channel Dev & Eval Set - REVERB Challenge')
+	parser.add_argument('--dataloc',      type=str,   help='Location for scp Files       (default: ./Data)', default='./Data')
+	parser.add_argument('--specImageloc', type=str,   help='Location for SpecImages      (default: ./SpecImages)', default='./SpecImages')
+	parser.add_argument('--destfolder',   type=str,   help='Location for enhanced audiofiles        (default: ./Enhanced)', default='./Enhanced')
+	parser.add_argument('--model',        type=str,   help='Absolute path for saved model           (default: ./chkpts/best.ckpt)', default='./chkpts/best.ckpt')
+	args = parser.parse_args()
+	
+	
+	device    = torch.device('cuda' if use_cuda else 'cpu')
+	model = SkipConvNet().to(device)
+	saved_model = torch.load(args.model)
+	model.load_state_dict(saved_model['state_dict'])
+	model.eval()
+	deocdeData(args.dataloc, args.specImageloc, args.destfolder, model, device)
 
